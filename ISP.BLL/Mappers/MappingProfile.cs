3ca7c8ee57a9c7ed63a0f76d02c2cb2a3c913890
artomrevus <artom_revus@epam.com>
2025-04-14 21:33:18 +0300

    Added mappings

--- ISP.BLL/Mappers/MappingProfile.cs
+++ ISP.BLL/Mappers/MappingProfile.cs
@@ -2,7 +2,14 @@ using AutoMapper;
 using ISP.BLL.DTOs.ISP.City;
 using ISP.BLL.DTOs.ISP.Client;
 using ISP.BLL.DTOs.ISP.ClientStatus;
+using ISP.BLL.DTOs.ISP.Employee;
 using ISP.BLL.DTOs.ISP.EmployeePosition;
+using ISP.BLL.DTOs.ISP.EmployeeStatus;
+using ISP.BLL.DTOs.ISP.House;
+using ISP.BLL.DTOs.ISP.Location;
+using ISP.BLL.DTOs.ISP.LocationType;
+using ISP.BLL.DTOs.ISP.Office;
+using ISP.BLL.DTOs.ISP.Street;
 using ISP.BLL.DTOs.Monitoring;
 using ISP.DAL.Entities;
 
@@ -14,13 +21,13 @@ public class MappingProfile : Profile
     {
         CreateMap<UserActivity, GetUserActivityDto>();
         
-        CreateMap<Client, GetClientDto>();
-        CreateMap<AddClientDto, Client>();
-        CreateMap<UpdateClientDto, Client>();
-        
         CreateMap<City, GetCityDto>();
         CreateMap<AddCityDto, City>();
         CreateMap<UpdateCityDto, City>();
+        
+        CreateMap<Client, GetClientDto>();
+        CreateMap<AddClientDto, Client>();
+        CreateMap<UpdateClientDto, Client>();
         
         CreateMap<ClientStatus, GetClientStatusDto>();
         CreateMap<AddClientStatusDto, ClientStatus>();
@@ -29,5 +36,33 @@ public class MappingProfile : Profile
         CreateMap<EmployeePosition, GetEmployeePositionDto>();
         CreateMap<AddEmployeePositionDto, EmployeePosition>();
         CreateMap<UpdateEmployeePositionDto, EmployeePosition>();
+        
+        CreateMap<Employee, GetEmployeeDto>();
+        CreateMap<AddEmployeeDto, Employee>();
+        CreateMap<UpdateEmployeeDto, Employee>();
+        
+        CreateMap<EmployeeStatus, GetEmployeeStatusDto>();
+        CreateMap<AddEmployeeStatusDto, EmployeeStatus>();
+        CreateMap<UpdateEmployeeStatusDto, EmployeeStatus>();
+        
+        CreateMap<House, GetHouseDto>();
+        CreateMap<AddHouseDto, House>();
+        CreateMap<UpdateHouseDto, House>();
+        
+        CreateMap<Location, GetLocationDto>();
+        CreateMap<AddLocationDto, Location>();
+        CreateMap<UpdateLocationDto, Location>();
+        
+        CreateMap<LocationType, GetLocationTypeDto>();
+        CreateMap<AddLocationTypeDto, LocationType>();
+        CreateMap<UpdateLocationTypeDto, LocationType>();
+        
+        CreateMap<Office, GetOfficeDto>();
+        CreateMap<AddOfficeDto, Office>();
+        CreateMap<UpdateOfficeDto, Office>();
+        
+        CreateMap<Street, GetStreetDto>();
+        CreateMap<AddStreetDto, Street>();
+        CreateMap<UpdateStreetDto, Street>();
     }
 }
